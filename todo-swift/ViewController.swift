82bc80dcf2d341d1cc3ca516faa9941e04a44229
ikaru19 <syafrizalizal17@gmail.com>
2025-01-09 11:14:48 +0700

    Init Commit
    Init Commit
    - Prepare Pods
    - Prepar xcode project

---
+++ todo-swift/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  todo-swift
+//
+//  Created by Admin on 09/01/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
