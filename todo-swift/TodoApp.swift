64a8c821bf241d86f87b9ecab77f2266bc548143
Syafrizal <syafrizalizal17@gmail.com>
2025-03-13 22:30:12 +0700

    Convert Project to SwiftUI

---
+++ todo-swift/TodoApp.swift
@@ -0,0 +1,22 @@
+//
+//  TodoApp.swift
+//  todo-swift
+//
+//  Created by Syafrizal on 13/03/25.
+//
+
+import SwiftUI
+import RealmSwift
+
+@main
+struct TodoApp: App {
+    
+    var body: some Scene {
+        WindowGroup {
+            // Replace the placeholder VStack with TaskListView
+            VStack {
+                Text("Hello World!")
+            }
+        }
+    }
+}
